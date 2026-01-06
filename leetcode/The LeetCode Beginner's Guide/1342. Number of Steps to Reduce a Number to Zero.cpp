7758194e25230d53c79f0da47a0fc918570648f7
Harman <harmanjss10@gmail.com>
2026-01-06 22:50:08 +0530

    Add personal notes and reflections in comments
    Added personal comments about solving the problem and recent experiences.

--- leetcode/The LeetCode Beginner's Guide/1342. Number of Steps to Reduce a Number to Zero.cpp
+++ leetcode/The LeetCode Beginner's Guide/1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -19,3 +19,4 @@ public:
 
 // I solved this question myself , not that thing to be proud of as it is very easy question but yeah feeling is great, finally meet Ayush afer a long time and spend some good quality time wuth him
 //College this sem till now is pretty go , lets see what'll happen next
+// Today i start playing basketball again and gave that crazy assist that i always dreamt of giving 
