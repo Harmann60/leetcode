024cb15beb895b6ce322e8dc884a243ba36607df
Harman <harmanjss10@gmail.com>
2026-01-06 16:23:43 +0530

    Add solution for Number of Steps to Reduce a Number to Zero
    Implement solution for counting steps to reduce a number to zero.

---
+++ leetcode/The LeetCode Beginner's Guide/1342. Number of Steps to Reduce a Number to Zero.cpp
@@ -0,0 +1,21 @@
+//6th January 2026
+// 1342. Number of Steps to Reduce a Number to Zero
+
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int step = 0;
+        while(num!=0)
+        {
+        if(num%2==0)
+        num/=2;
+        else
+        num-=1;
+        step++ ;
+    }
+    return step;
+    }
+};
+
+// I solved this question myself , not that thing to be proud of as it is very easy question but yeah feeling is great, finally meet Ayush afer a long time and spend some good quality time wuth him
+//College this sem till now is pretty go , lets see what'll happen next
