902bb59d4eab03ff69cdd4c7e5bc9ee05269d5fa
Harman <harmanjss10@gmail.com>
2026-02-05 13:49:08 +0530

    Revise SQL query and comments in Find Customer Refree
    Updated the SQL file with comments and query for finding customers without a referee or with a referee ID not equal to 2.

---
+++ leetcode/Sql50/584. Find Customer Refree.sql
@@ -0,0 +1,9 @@
+# 5th February 2026
+# 584. Find Customer Refree
+# Write your MySQL query statement below
+select name from Customer
+where referee_id is NULL or referee_id != 2;
+
+# After alot of efforts and fck ups in this simple question and finally solve this while Ayush was teasing me 
+# and questioning my abilities and today is also last day of one of our favourite teacher because this sem i scored good marks in first two
+# assignment then now mam is leaving 
