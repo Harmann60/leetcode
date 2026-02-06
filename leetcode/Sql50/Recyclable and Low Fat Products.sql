03c79178bc7670371d81819ff67081a9413ee54a
Harman <harmanjss10@gmail.com>
2026-02-06 21:51:58 +0530

    Rename SQL file for recyclable and low fat products

---
+++ leetcode/Sql50/Recyclable and Low Fat Products.sql
@@ -0,0 +1,6 @@
+##Recyclable and Low Fat Products
+## 6, February 2025
+  
+select product_id from Products where low_fats='Y' AND recyclable='Y';
+
+## Solving leetcode question in the first go this is new for me RCB women won wpl again i also played first time out of SIT in flames 
