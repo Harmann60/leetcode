8c44399627224c34302c97e4af93b8f432719a9d
Harman <harmanjss10@gmail.com>
2026-02-07 17:00:10 +0530

    Create Big Countries.sql

---
+++ leetcode/Sql50/Big Countries.sql
@@ -0,0 +1,9 @@
+# Big Countries
+# 7 February 2026
+
+Select name, population, area from World 
+Where area>3000000 or population > 25000000
+
+# It was Preety fun last day of DSRW assessment like I found out I love jumping for the ball and clear it 
+# Ayush is coming home for the match spurs vs United 
+# Come on you Spurs
