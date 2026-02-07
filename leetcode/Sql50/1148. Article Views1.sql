23ecd9e06ef3c5bc749da5ac612d13dcafbbe3ec
Harman <harmanjss10@gmail.com>
2026-02-08 02:37:54 +0530

    Enhance Article Views1.sql with comments
    Added metadata and motivational comment to the SQL file.

---
+++ leetcode/Sql50/1148. Article Views1.sql
@@ -0,0 +1,6 @@
+# 1148. Article Views1.sql
+# 8 February 2026
+
+select distinct author_id as id from Views where author_id=viewer_id order by author_id;
+
+#Just keep working on your goals helping other even if you are reason behing someone smile be the reason
