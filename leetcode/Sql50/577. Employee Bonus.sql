c34efb3f9f53451cf66185da466432d2f4bbbbe3
Harman <harmanjss10@gmail.com>
2026-02-14 12:25:38 +0530

    Add comments and notes to Employee Bonus SQL file
    Added metadata comments and a birthday note to the SQL file.

---
+++ leetcode/Sql50/577. Employee Bonus.sql
@@ -0,0 +1,6 @@
+#577. Employee Bonus
+#14 February 2026
+  
+select e.name,b.bonus from Employee e left join Bonus b on e.empID = b.empId where b.bonus is NULL or b.bonus <1000;
+
+#Today is Abodh's Birthday we all are going there , How to sell drugs fast online what an amazing series 
