215d02711becf22fc6d2fe74bfcf00eee7daa52b
Harman <harmanjss10@gmail.com>
2026-02-17 01:00:59 +0530

    Add comments and metadata to Not Boring Movies SQL
    Added metadata and comments to the SQL query.

--- leetcode/Sql50/620.Not Boring Moives.sql
+++ leetcode/Sql50/620.Not Boring Moives.sql
@@ -1,1 +1,6 @@
+#620. Not Boring Movies
+#16 Feburary 2026
+  
 select id,movie,description,rating from Cinema where id%2 = 1 and description!='boring' order by rating desc;
+
+#Abodh's Birthday was quite fun
