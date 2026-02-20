c5ee82b114d0c7e6ceebcbe218d76f660b01ff69
Harman <harmanjss10@gmail.com>
2026-02-20 22:26:05 +0530

    Create 596. Classes with at least 5 Students.sql

---
+++ leetcode/Sql50/596. Classes with at least 5 Students.sql
@@ -0,0 +1,6 @@
+# 596. Classes with at least 5 Students
+# 20 Feburary 2026
+  
+select class from Courses group by 1 having Count(student)>4;
+
+#Today our team won the match by 20 points Shaq bhaiya good memories 
