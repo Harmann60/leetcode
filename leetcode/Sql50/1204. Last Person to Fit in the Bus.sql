5dd075cfc2f93c3a63dc0426da3843521bd4ff7d
Harman <harmanjss10@gmail.com>
2026-03-09 17:24:29 +0530

    Create 1204. Last Person to Fit in the Bus.sql

---
+++ leetcode/Sql50/1204. Last Person to Fit in the Bus.sql
@@ -0,0 +1,9 @@
+# 9 March 2026
+# 1204. Last Person to Fit in the Bus
+  
+Select a.person_name from Queue a join Queue b on a.turn>=b.turn group by a.turn having Sum(b.weight)<=1000 order by a.turn Desc LIMIT 1;
+
+#I want to write something i dont know how to express this feeling like i made my happy birthday repo public and made that as a template so people can make their loved one feels special now i checked back at
+# that repo and find that people are actually doing that wesian or a couple from puerto rico i achieve my goal it bring happiness in thier life
+# What so if it can bring joy to my life but same thing bringing joy to others and thats the whole point ("If someone is smiling just beacuse of your smallest effort, be proud of yourself")
+# dont'worry not for us one day it will 
